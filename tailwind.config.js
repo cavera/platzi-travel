b3fa103c4d998ea386df697c9b93bc653298903e
cavera <cavera.de@gmail.com>
2022-09-02 16:17:14 -0500

    resultado del curso de tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,41 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ["./public/index.html", "./src/**/*.{html,js}"],
+	darkMode: "class",
+	theme: {
+		extend: {
+			backgroundImage: {
+				bali: "url('../img/bali.jpg')",
+				chicago: "url('../img/chicago.jpg')",
+				europe: "url('../img/europe.jpg')",
+				iceland: "url('../img/iceland.jpg')",
+				LA: "url('../img/LA.jpg')",
+				miami: "url('../img/miami.jpg')",
+				new_york: "url('../img/new_york.jpg')",
+				norway: "url('../img/norway.jpg')",
+				sanFrancisco: "url('../img/sanFrancisco.jpg')",
+				sanFranciscoDesktop: "url('../img/sanFranciscoDesktop.jpg')",
+				seattle: "url('../img/seattle.jpg')",
+				switzerland: "url('../img/switzerland.jpg')",
+				sydney: "url('../img/sydney.jpg')",
+				yosemite: "url('../img/yosemite.jpg')",
+			},
+			backgroundColor: (theme) => ({
+				...theme("colors"),
+				primary: "#CC2D4A",
+				secondary: "#8FA206",
+				tertiary: "#61AEC9",
+			}),
+			textColor: (theme) => ({
+				...theme("colors"),
+				primary: "#CC2D4A",
+				secondary: "#8FA206",
+				tertiary: "#61AEC9",
+			}),
+			fontFamily: {
+				Montserrat: ["Montserrat", "sans-serif"],
+			},
+		},
+	},
+	plugins: [],
+};
